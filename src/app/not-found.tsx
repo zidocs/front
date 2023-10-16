e37dd5780596caceb096ec4282b6a7cd4c5c52af
William Jonathan <willyeager01@gmail.com>
2023-10-16 01:21:01 -0300

    chore: rollback not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,13 @@
-// import config from '../../public/starter-kit/zidocs.json';
+'use client';
 
-// import { useEffect } from 'react';
+import config from '../../public/starter-kit/zidocs.json';
+
+import { useEffect } from 'react';
 
 export default function NotFound() {
-  // useEffect(() => {
-  //   window.location.replace(`/${config.navigation[0].pages[0]}`);
-  // });
+  useEffect(() => {
+    window.location.replace(`/${config.navigation[0].pages[0]}`);
+  });
 
-  return <div>NOT FOUND</div>;
+  return null;
 }
